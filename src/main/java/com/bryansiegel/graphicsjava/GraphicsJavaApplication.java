3284145fabf0a13d12539cdc5ed91101e031b703
Bryan Siegel <bryansiegel@gmail.com>
2024-05-16 15:43:35 -0700

    initial commit

---
+++ src/main/java/com/bryansiegel/graphicsjava/GraphicsJavaApplication.java
@@ -0,0 +1,13 @@
+package com.bryansiegel.graphicsjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphicsJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphicsJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bryansiegel/graphicsjava/GraphicsJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bryansiegel.graphicsjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphicsJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
