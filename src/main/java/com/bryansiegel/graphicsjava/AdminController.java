994cbbf2d9544d600f4c32d4fb309a275872fd1f
Bryan Siegel <bryansiegel@gmail.com>
2024-05-16 16:38:32 -0700

    login to dashboard with spring security

---
+++ src/main/java/com/bryansiegel/graphicsjava/AdminController.java
@@ -0,0 +1,18 @@
+package com.bryansiegel.graphicsjava;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController {
+
+    @GetMapping("/login")
+    String login() {
+        return "admin/login";
+    }
+
+    @GetMapping("/admin/dashboard")
+    String dashboard() {
+        return "admin/dashboard";
+    }
+}
