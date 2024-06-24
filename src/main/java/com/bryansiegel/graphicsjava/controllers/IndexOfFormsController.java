636d0431cf67b634f887c9e8f0662b3582066c84
Bryan Siegel <bryansiegel@gmail.com>
2024-06-24 07:23:58 -0700

    indexofformsupdate

--- src/main/java/com/bryansiegel/graphicsjava/controllers/IndexOfFormsController.java
+++ src/main/java/com/bryansiegel/graphicsjava/controllers/IndexOfFormsController.java
@@ -1,6 +1,7 @@
 package com.bryansiegel.graphicsjava.controllers;
 
 import com.bryansiegel.graphicsjava.dtos.IndexOfFormsDto;
+import com.bryansiegel.graphicsjava.models.FormDownloadsModel;
 import com.bryansiegel.graphicsjava.models.IndexOfFormsModel;
 import com.bryansiegel.graphicsjava.repositories.indexOfFormsRepository;
 import jakarta.validation.Valid;
@@ -18,6 +19,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.Date;
+import java.util.Optional;
 
 @Controller
 public class IndexOfFormsController {
@@ -114,48 +116,82 @@ public class IndexOfFormsController {
 
     //Update
     @PostMapping("/admin/index-of-forms/update/{id}")
-    public String updateIndexOfForms(@Valid @ModelAttribute IndexOfFormsDto indexOfFormsDto, @PathVariable Long id, @RequestParam String formName, BindingResult result) {
+    public String updateIndexOfForms(@Valid @ModelAttribute IndexOfFormsDto indexOfFormsDto, @PathVariable Long id, @RequestParam String formName, @RequestParam("file") MultipartFile file, BindingResult result) {
 
-        if (indexOfFormsDto.getFile().isEmpty()) {
-            result.addError(new FieldError("indexOfFormsDto", "file", "The image file is required"));
-        }
+//        if (indexOfFormsDto.getFile().isEmpty()) {
+//            result.addError(new FieldError("indexOfFormsDto", "file", "The image file is required"));
+//        }
 
         if (result.hasErrors()) {
             return "admin/index-of-forms/edit.html";
         }
 
-        //save file
-        MultipartFile file = indexOfFormsDto.getFile();
-        Date createdAt = new Date();
-        String storageFileName = createdAt.getTime() + "_" + file.getOriginalFilename();
+        Optional<IndexOfFormsModel> optionalIndexOfFormsModel = repo.findById(id);
+
+        if (optionalIndexOfFormsModel.isPresent()) {
+            Date createdAt = new Date();
+            String storageFileName = createdAt.getTime() + "_" + file.getOriginalFilename();
 
 
-        //SET FilePath
-        String filePath = UPLOAD_DIR + storageFileName;
+            //SET FilePath
+            String filePath = "files/form-downloads/" + storageFileName;
 
-        try {
-            Path uploadPath = Paths.get(UPLOAD_DIR);
+            try {
+                Path uploadPath = Paths.get(UPLOAD_DIR);
 
-            if (!Files.exists(uploadPath)) {
-                Files.createDirectories(uploadPath);
+                if (!Files.exists(uploadPath)) {
+                    Files.createDirectories(uploadPath);
+                }
+
+                try (InputStream inputStream = file.getInputStream()) {
+                    Files.copy(inputStream, Paths.get(UPLOAD_DIR + storageFileName), StandardCopyOption.REPLACE_EXISTING);
+
+                    //Save to db
+                    IndexOfFormsModel indexOfFormsModel = optionalIndexOfFormsModel.get();
+                    indexOfFormsModel.setFormName(formName);
+                    indexOfFormsModel.setFilePath(filePath);
+
+                    repo.save(indexOfFormsModel);
+                }
+            } catch (Exception ex) {
+                System.out.println("Exception: " + ex.getMessage());
             }
-
-            try (InputStream inputStream = file.getInputStream()) {
-                Files.copy(inputStream, Paths.get(UPLOAD_DIR + storageFileName), StandardCopyOption.REPLACE_EXISTING);
-
-                //Save to db
-                IndexOfFormsModel _indexofforms = repo.findById(id)
-                        .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
-
-
-                _indexofforms.setFormName(formName);
-                _indexofforms.setFilePath(filePath);
-
-                repo.save(_indexofforms);
-            }
-        } catch (Exception ex) {
-            System.out.println("Exception: " + ex.getMessage());
         }
+
+        //save file
+//        MultipartFile file = indexOfFormsDto.getFile();
+//        Date createdAt = new Date();
+//        String storageFileName = createdAt.getTime() + "_" + file.getOriginalFilename();
+//
+//
+//        //SET FilePath
+//        String filePath = UPLOAD_DIR + storageFileName;
+//
+//        try {
+//            Path uploadPath = Paths.get(UPLOAD_DIR);
+//
+//            if (!Files.exists(uploadPath)) {
+//                Files.createDirectories(uploadPath);
+//            }
+//
+//            try (InputStream inputStream = file.getInputStream()) {
+//                Files.copy(inputStream, Paths.get(UPLOAD_DIR + storageFileName), StandardCopyOption.REPLACE_EXISTING);
+//
+//                //Save to db
+//                IndexOfFormsModel _indexofforms = repo.findById(id)
+//                        .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+//
+//
+//                _indexofforms.setFormName(formName);
+//                _indexofforms.setFilePath(filePath);
+//
+//                repo.save(_indexofforms);
+//            }
+//        } catch (Exception ex) {
+//            System.out.println("Exception: " + ex.getMessage());
+//        }
+
+
         return "redirect:/admin/index-of-forms/";
     }
 
